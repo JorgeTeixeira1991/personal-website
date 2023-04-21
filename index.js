ad5f0078c08c58723d6f47e2e919edc70bbf5829
Jorge Teixeira <jorge555444@gmail.com>
2023-04-21 12:26:10 +0100

    modals done

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
-function aboutMePopUp() {
-  let popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+
+document.addEventListener("DOMContentLoaded", function() {
+  var about = showAboutModal();
+  var projects = showProjectsModal();
+  var notes = showNotesModal();
+  closeModalOnBackgroundClick(about, projects, notes);
+})
+
+function showAboutModal() {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  return modal;
+}
+
+function showProjectsModal() {
+  var modal_projects = document.getElementById("projects-modal");
+  var projects_btn = document.getElementById("projects-btn");
+  var span = document.getElementsByClassName("close")[0];
+  
+  projects_btn.onclick = function() {
+    modal_projects.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal_projects.style.display = "none";
+  }
+  return modal_projects;
+}
+
+function showNotesModal() {
+  var notes_modal = document.getElementById("notes-modal");
+  var notes_btn = document.getElementById("notes-btn");
+  var span = document.getElementsByClassName("close")[0];
+  
+  notes_btn.onclick = function() {
+    notes_modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+    notes_modal.style.display = "none";
+  }
+  return notes_modal;
+}
+
+function closeModalOnBackgroundClick(modal1, modal2, modal3) {
+  window.onclick = function(event) {
+    switch(event.target){
+      case modal1:
+        modal1.style.display = "none";
+      case modal2:
+        modal2.style.display = "none";
+      case modal3:
+        modal3.style.display = "none";
+    }
+  }  
 }
